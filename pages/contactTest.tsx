1a665b7db72949254c22eeebb908290256dda7db
OsCordero <oscarcordero97@gmail.com>
2021-11-11 09:11:37 -0600

    🎉 initial commit

---
+++ pages/contactTest.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Link from "next/link";
+
+const ContactText = () => {
+  return (
+    <div>
+      <h1>Questions? ping us:</h1>
+      <Link href="/">Home</Link>
+    </div>
+  );
+};
+
+export default ContactText;
